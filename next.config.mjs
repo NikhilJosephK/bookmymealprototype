7a91af90086c0c68fc1d659a6e7174347f5d5012
NikhilJosephK <nikhilnjk@gmail.com>
2024-12-27 01:14:34 +0530

    delete middleware

--- next.config.mjs
+++ next.config.mjs
@@ -1,26 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    // async headers() {
-    //     return [
-    //         {
-    //             source: '/api/:path*', // Apply to all API routes
-    //             headers: [
-    //                 {
-    //                     key: 'Access-Control-Allow-Origin',
-    //                     value: '*' // Or specify your frontend domain here
-    //                 },
-    //                 {
-    //                     key: 'Access-Control-Allow-Methods',
-    //                     value: 'GET, POST, PUT, DELETE, OPTIONS'
-    //                 },
-    //                 {
-    //                     key: 'Access-Control-Allow-Headers',
-    //                     value: 'Content-Type, Authorization'
-    //                 }
-    //             ]
-    //         }
-    //     ];
-    // }
-};
+const nextConfig = {};
 
 export default nextConfig;
