1c9a648ba3ffc9c01f16d91a86f4c2479bbed4a8
NikhilJosephK <nikhilnjk@gmail.com>
2024-12-27 00:43:23 +0530

    Refactor cors middleware and configure Access-Control-Allow headers in next.config.js

--- next.config.mjs
+++ next.config.mjs
@@ -1,26 +1,26 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    async headers() {
-        return [
-            {
-                source: '/api/:path*', // Apply to all API routes
-                headers: [
-                    {
-                        key: 'Access-Control-Allow-Origin',
-                        value: '*' // Or specify your frontend domain here
-                    },
-                    {
-                        key: 'Access-Control-Allow-Methods',
-                        value: 'GET, POST, PUT, DELETE, OPTIONS'
-                    },
-                    {
-                        key: 'Access-Control-Allow-Headers',
-                        value: 'Content-Type, Authorization'
-                    }
-                ]
-            }
-        ];
-    }
+    // async headers() {
+    //     return [
+    //         {
+    //             source: '/api/:path*', // Apply to all API routes
+    //             headers: [
+    //                 {
+    //                     key: 'Access-Control-Allow-Origin',
+    //                     value: '*' // Or specify your frontend domain here
+    //                 },
+    //                 {
+    //                     key: 'Access-Control-Allow-Methods',
+    //                     value: 'GET, POST, PUT, DELETE, OPTIONS'
+    //                 },
+    //                 {
+    //                     key: 'Access-Control-Allow-Headers',
+    //                     value: 'Content-Type, Authorization'
+    //                 }
+    //             ]
+    //         }
+    //     ];
+    // }
 };
 
 export default nextConfig;
